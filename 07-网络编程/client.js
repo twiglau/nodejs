af29b2602b6cd7fab6f11fce3ec46d9c2024849e
lau <twigliu@hotmail.com>
2021-10-14 14:23:29 +0800

    网络

--- 07-网络编程/client.js
+++ 07-网络编程/client.js
@@ -3,7 +3,6 @@ var client = net.connect({port:8124},function(){ // 'connect': listener
     console.log('client connected');
     client.write('world!\r\n');
 });
-
 client.on('data',function(data){
     console.log(data.toString());
     client.end();
